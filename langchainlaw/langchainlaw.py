ff75464961192a45ad2680f64ce735650c080100
Mike Lynch <m.lynch@sydney.edu.au>
2023-10-03 15:27:26 +1100

    Refactored prompts into a CaseChat class which can return a set of langchain messages to be passed to openAI
    First working version for evaluation

--- langchainlaw/langchainlaw.py
+++ langchainlaw/langchainlaw.py
@@ -1,11 +1,12 @@
 import argparse
 import json
+import sys
 from pathlib import Path
-from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
 from openpyxl import Workbook
 
 
-from langchainlaw.prompts import load_prompts
+from langchainlaw.prompts import CaseChat
 
 
 def load_config(cf_file):
@@ -14,11 +15,11 @@ def load_config(cf_file):
         return cf
 
 
-def load_case(jsonfile):
-    with open(jsonfile, "r") as file:
-        data = json.load(file)
-    judgment = data.pop("judgment")
-    return data, judgment
+def load_case(casefile):
+    with open(casefile, "r") as file:
+        return json.load(file)
+    # judgment = data.pop("judgment")
+    # return judgment
 
 
 def classify():
@@ -36,6 +37,18 @@ def classify():
         help="Run without making calls to OpenAI, for testing prompts",
     )
     ap.add_argument(
+        "--case",
+        default="",
+        type=str,
+        help="Select a single case by its filename",
+    )
+    ap.add_argument(
+        "--prompt",
+        default="",
+        type=str,
+        help="Generate results from only one prompt",
+    )
+    ap.add_argument(
         "--output",
         default="./results.xlsx",
         type=Path,
@@ -43,32 +56,91 @@ def classify():
     )
     args = ap.parse_args()
     cf = load_config(args.config)
-    prompts = load_prompts(cf["PROMPTS"])
-    llm = OpenAI(
-        openai_api_key=cf["OPENAI_API_KEY"],
-        openai_organization=cf["OPENAI_ORGANIZATION"],
-        temperature=cf["TEMPERATURE"],
-    )
+    prompts = CaseChat(cf["PROMPTS"])
+    if args.prompt:
+        if not prompts.prompt(args.prompt):
+            print(f"No prompt defined with name '{args.prompt}'")
+            sys.exit()
+    chat = None
+    if not args.test:
+        chat = ChatOpenAI(
+            model_name=cf["OPENAI_CHAT_MODEL"],
+            openai_api_key=cf["OPENAI_API_KEY"],
+            openai_organization=cf["OPENAI_ORGANIZATION"],
+            temperature=cf["TEMPERATURE"],
+        )
 
     workbook = Workbook()
     worksheet = workbook.active
 
-    worksheet.append(["file", "mnc"] + [p.name for p in prompts])
+    if not args.prompt:
+        worksheet.append(["file", "mnc"] + prompts.prompt_names)
 
-    for jsonfile in Path(cf["JUDGMENTS"]).glob("*.json"):
-        metadata, judgment = load_case(jsonfile)
-        print("Case: " + metadata["mnc"])
-        results = [str(jsonfile), metadata["mnc"]]
-        for prompt in prompts:
-            prompt_str = prompt.make_prompt(metadata, judgment[:10])
-            if args.test:
-                results.append(prompt_str)
-            else:
-                result = llm(prompt_str)
-                results.append(result)
-        worksheet.append(results)
+    if args.case:
+        case = Path(cf["INPUT"]) / Path(args.case)
+        if not case.is_file():
+            print(f"Case file {args.case} not found")
+            sys.exit()
+        cases = [case]
+    else:
+        cases = Path(cf["INPUT"]).glob("*.json")
 
-    workbook.save(args.output)
+    for casefile in cases:
+        judgment = load_case(casefile)
+        print("Case: " + judgment["mnc"])
+        # results = [str(casefile), judgment["mnc"]]
+
+        system_prompt = prompts.start_chat()
+
+        if not args.test:
+            response = chat([system_prompt])
+
+        # message = prompts.start_judgment(judgment)
+        # print(message)
+        # print("\n...\n\n")
+
+        # if not args.test:
+        #     response = chat([message])
+        #     print(response)
+        #     print("\n----\n\n")
+
+        for message in prompts.next_prompt(judgment):
+            if not args.test:
+                try:
+                    response = chat([message])
+                    try:
+                        json_response = json.loads(response.content)
+                        outfile = Path(cf["OUTPUT"]) / Path(casefile.name)
+                        with open(outfile, "w") as fh:
+                            json.dump(json_response, fh, indent=2)
+                    except json.decoder.JSONDecodeError as e:
+                        textout = outfile.with_suffix(".txt")
+                        print(f"invalid JSON in response: {e}")
+                        print(f"writing raw content to {textout}")
+                        with open(textout, "w") as fh:
+                            fh.write(response.content)
+                except Exception as e:
+                    print(f"Error classifying {casefile}: {e}")
+
+    #     for prompt in prompts:
+    #         prompt_str = prompt.make_prompt(metadata, judgment)
+    #         output = None
+    #         if args.test:
+    #             output = prompt_str
+    #         else:
+    #             output = chat(prompt_str)
+    #             if prompt.multiple:
+    #                 for follow_up in prompt.multiple_prompt(output):
+    #                     more_output = llm(prompt_str)
+    #         if args.prompt:
+    #             print(output)
+    #         else:
+    #             results.append(output)
+    #     if not args.prompt:
+    #         worksheet.append(results)
+
+    # if not args.prompt:
+    #     workbook.save(args.output)
 
 
 if __name__ == "__main__":
--- langchainlaw/prompts.py
+++ langchainlaw/prompts.py
@@ -1,33 +1,81 @@
 from dataclasses import dataclass
 import yaml
+import json
+
+from langchain.schema import HumanMessage, SystemMessage
 
 
 @dataclass
 class CasePrompt:
     name: str
     prompt: str
-    preamble: str = ""
-    structure: str = ""
-    followup: str = ""
+    multiple: str
 
-    def make_prompt(self, metadata, judgment):
-        prompt = self.preamble.format(metadata=metadata, judgment=judgment)
-        prompt = prompt + self.prompt
-        if self.structure:
-            prompt = prompt + self.structure
-        return prompt
+    def message(self, judgment):
+        content = self.prompt.format(judgment=json.dumps(judgment))
+        return HumanMessage(content=content)
 
 
-def load_prompts(prompt_yaml):
-    prompts = []
-    with open(prompt_yaml, "r") as fh:
-        prompt_cf = yaml.load(fh, Loader=yaml.Loader)
-        preamble = prompt_cf["preamble"]
-        for p in prompt_cf["prompts"]:
-            prompt = CasePrompt(
-                name=p["name"],
-                prompt=p["prompt"],
-                preamble=preamble,
-            )
-            prompts.append(prompt)
-    return prompts
+class CaseChat:
+    def __init__(self, yaml=None):
+        self._system = None
+        self._judgment = None
+        self._prompts = {}
+        self._prompt_names = []
+        if yaml is not None:
+            self.load_yaml(yaml)
+
+    @property
+    def system(self):
+        return self._system
+
+    @property
+    def judgment(self):
+        return self._judgment
+
+    @property
+    def prompt_names(self):
+        return self._prompt_names
+
+    def prompt(self, name):
+        return self._prompts.get(name, None)
+
+    def add_prompt(self, name, prompt, multiple):
+        if name in self._prompts:
+            raise ValueError(f"Prompt with name {name} already defined")
+        self._prompt_names.append(name)
+        self._prompts[name] = CasePrompt(
+            name=name,
+            prompt=prompt,
+            multiple=multiple,
+        )
+
+    def load_yaml(self, yaml_file):
+        with open(yaml_file, "r") as fh:
+            prompt_cf = yaml.load(fh, Loader=yaml.Loader)
+            self._system = prompt_cf["system"]
+            self._judgment = prompt_cf["judgment"]
+            for p in prompt_cf["prompts"]:
+                self.add_prompt(
+                    p["name"],
+                    p["prompt"],
+                    p.get("multiple", None),
+                )
+
+    def start_chat(self):
+        return SystemMessage(content=self.system)
+
+    def start_judgment(self, judgment):
+        return HumanMessage(content=self.judgment.format(judgment=judgment))
+
+    def next_prompt(self, judgment):
+        for prompt_name in self._prompt_names:
+            yield self._prompts[prompt_name].message(judgment)
+
+    def multiple_prompt(self, response):
+        try:
+            responses = json.loads(response)
+        except json.decoder.JSONDecodeError:
+            return ""
+        for x in responses:
+            yield self.multiple.format(x=x)
