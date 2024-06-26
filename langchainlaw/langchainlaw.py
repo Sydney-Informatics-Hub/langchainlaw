8d6342672da5a3bc4fe00e582e2250047935c6f1
Mike Lynch <m.lynch@sydney.edu.au>
2024-06-26 11:48:58 +1000

    Renamed collimate to as_columns and flatten to as_dict, and Classifier now sends progress reports to stdout unless given a "quiet" flag

--- langchainlaw/langchainlaw.py
+++ langchainlaw/langchainlaw.py
@@ -16,7 +16,7 @@ class Classifier:
     """Class which wraps up the case classifier. Config is a JSON object -
     see config.example.json"""
 
-    def __init__(self, config):
+    def __init__(self, config, quiet=False):
         self.provider = config["provider"]
         try:
             self.api_cf = config["providers"][self.provider]
@@ -30,7 +30,7 @@ class Classifier:
         self.headers = ["file", "mnc"]
         for prompt in self.prompts.next_prompt():
             self.headers.extend(prompt.headers)
-
+        self.quiet = quiet
         self.chat = ChatOpenAI(
             model_name=self.api_cf["model"],
             openai_api_key=self.api_cf["api_key"],
@@ -49,6 +49,11 @@ class Classifier:
         with open(casefile, "r") as file:
             return json.load(file)
 
+    def message(self, str):
+        """Print some progress info unless set to quiet mode"""
+        if not self.quiet:
+            print(str)
+
     def run_prompt(self, case_id, prompt):
         """Actually send prompt to LLM, unless there's already a response in the
         cache.
@@ -65,13 +70,20 @@ class Classifier:
             if self.test:
                 if self.cache:
                     response = self.cache.read(case_id, prompt.name)
-                if not response:
+                if response is not None:
+                    self.message(f"[{case_id}] {prompt.name} - cached result")
+                else:
+                    self.message(f"[{case_id}] {prompt.name} - mock result")
                     response = prompt.mock_response()
             else:
                 if self.cache:
                     response = self.cache.read(case_id, prompt.name)
-                if response is None:
+                if response is not None:
+                    self.message(f"[{case_id}] {prompt.name} - cached result")
+                else:
+                    self.message(f"[{case_id}] {prompt.name} - asking LLM")
                     response = self.chat([message]).content
+                    self.message(f"[{case_id}] pausing for {self.rate_limit}")
                     time.sleep(self.rate_limit)
         except Exception as e:
             if self.cache:
@@ -85,7 +97,7 @@ class Classifier:
         """Collimate one set of results."""
         return self.prompts.prompt(name).collimate(results)
 
-    def collimate(self, results):
+    def as_columns(self, results):
         """Take the dict of results returned by classify and aligns it
         with the column headers from the prompts"""
         cols = [results["file"], results["mnc"]]
@@ -93,7 +105,9 @@ class Classifier:
             cols.extend(self.collimate_one(name, results.get(name, None)))
         return cols
 
-    def flatten(self, results):
+    def as_dict(self, results):
+        """Takes the dict of results returned by classify and returns a
+        flattened dict (no nesting, keys are the same as prompts.headers)"""
         d = {"file": results["file"], "mnc": results["mnc"]}
         for name in self.prompts.prompt_names:
             r = self.prompts.prompt(name).flatten(results[name])
@@ -101,7 +115,7 @@ class Classifier:
                 d[k] = v
         return d
 
-    def classify(self, casefile, test=False, one_prompt=None):
+    def classify(self, casefile, test=False, one_prompt=None, quiet=False):
         """Run the classifier for a single case and returns the results as a
         dict by prompt label."""
         self.test = test
@@ -109,6 +123,8 @@ class Classifier:
         self.prompts.judgment = self.load_case(casefile)
         results = {"file": str(casefile), "mnc": self.prompts.judgment["mnc"]}
 
+        if not quiet:
+            print(f"[{case_id}] Classifying...")
         system_prompt = self.prompts.start_chat()
 
         if not self.test:
@@ -116,6 +132,8 @@ class Classifier:
 
         for prompt in self.prompts.next_prompt():
             if not one_prompt or prompt.name == one_prompt:
+                if not quiet:
+                    print(f"[{case_id}] {prompt.name}")
                 results[prompt.name] = self.run_prompt(case_id, prompt)
         return results
 
